dd6fc2ecd0122601eb6d502dcf9d1784f35e29e4
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-04-26 23:14:33 +0100

    Add option for CompactFishing to show catches immediately and add icons/xp as messages are received
    Also refactors the existing functionality a little to make supporting both easier.

---
+++ src/main/java/uk/mrjamesco/jamboree/mixins/ChatComponentMixin.java
@@ -0,0 +1,18 @@
+package uk.mrjamesco.jamboree.mixins;
+
+import net.minecraft.client.GuiMessage;
+import net.minecraft.client.gui.components.ChatComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.List;
+
+@Mixin(ChatComponent.class)
+public interface ChatComponentMixin {
+    @Accessor
+    List<GuiMessage> getAllMessages();
+
+    @Invoker("refreshTrimmedMessages")
+    void refreshTrimmedMessages();
+}
