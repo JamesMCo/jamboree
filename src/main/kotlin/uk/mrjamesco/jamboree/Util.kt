afb3fbef09fa07be3a3d762c35eec7d88cc9c382
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-05-05 18:23:26 +0100

    Move Noxesium initialisation to Util object

--- src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
@@ -1,6 +1,8 @@
 package uk.mrjamesco.jamboree
 
+import com.noxcrew.noxesium.NoxesiumFabricMod
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
+import net.fabricmc.loader.api.FabricLoader
 import uk.mrjamesco.jamboree.Jamboree.Companion.logger
 
 object Util {
@@ -12,5 +14,10 @@ object Util {
 
         // Detect joining MCC Island
         ClientPlayConnectionEvents.JOIN.register { handler, _, _ -> onMCCIsland = Regex("mccisland\\.(net|com)").containsMatchIn(handler.connection.remoteAddress.toString()) }
+
+        // Initialise Noxesium once if it's loaded
+        if (FabricLoader.getInstance().isModLoaded("noxesium")) {
+            NoxesiumFabricMod.initialize()
+        }
     }
 }
--- src/main/kotlin/uk/mrjamesco/jamboree/compactfishing/CompactFishing.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/compactfishing/CompactFishing.kt
@@ -1,6 +1,5 @@
 package uk.mrjamesco.jamboree.compactfishing
 
-import com.noxcrew.noxesium.NoxesiumFabricMod
 import com.noxcrew.noxesium.network.NoxesiumPackets
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
@@ -49,7 +48,6 @@ object CompactFishing {
         logger.info("Registering CompactFishing listeners")
 
         // Detect being in a fishing server
-        NoxesiumFabricMod.initialize()
         NoxesiumPackets.CLIENT_MCC_SERVER.addListener(this) { _, packet, _ -> onFishingIsland = (packet.serverType == "lobby" && Regex("^(temperate|tropical|barren)_.+").matches(packet.subType)) }
 
         // Detect fishing messages
