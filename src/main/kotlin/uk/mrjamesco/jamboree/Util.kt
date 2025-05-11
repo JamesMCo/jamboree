887f450ed70dc7342bfbe797136738fa2695737b
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-05-11 22:35:05 +0100

    Fix cherrypicking Util window flash functionality from 1.21.4 branch also including Noxesium initialisation
    This isn't needed on the 1.21.1 version of Noxesium being used, and shouldn't have been included. Whoops.

--- src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
@@ -1,9 +1,7 @@
 package uk.mrjamesco.jamboree
 
 import com.mojang.blaze3d.platform.Window
-import com.noxcrew.noxesium.NoxesiumFabricMod
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
-import net.fabricmc.loader.api.FabricLoader
 import org.lwjgl.glfw.GLFW
 import uk.mrjamesco.jamboree.Jamboree.Companion.logger
 
@@ -16,11 +14,6 @@ object Util {
 
         // Detect joining MCC Island
         ClientPlayConnectionEvents.JOIN.register { handler, _, _ -> onMCCIsland = Regex("mccisland\\.(net|com)").containsMatchIn(handler.connection.remoteAddress.toString()) }
-
-        // Initialise Noxesium once if it's loaded
-        if (FabricLoader.getInstance().isModLoaded("noxesium")) {
-            NoxesiumFabricMod.initialize()
-        }
     }
 
     val Window.isActive: Boolean
