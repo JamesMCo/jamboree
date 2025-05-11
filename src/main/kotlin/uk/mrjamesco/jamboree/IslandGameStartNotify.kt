04adef8cfc7ff85ef273a1720f05cdb09a893812
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-05-11 22:26:05 +0100

    Add window flash functionality to chat ding
    Also moves the existing window flash functionality from island game start notify to the util object

--- src/main/kotlin/uk/mrjamesco/jamboree/IslandGameStartNotify.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/IslandGameStartNotify.kt
@@ -1,18 +1,12 @@
 package uk.mrjamesco.jamboree
 
-import com.mojang.blaze3d.platform.Window
 import com.noxcrew.noxesium.network.NoxesiumPackets
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.Minecraft
-import org.lwjgl.glfw.GLFW
 import uk.mrjamesco.jamboree.Jamboree.Companion.logger
+import uk.mrjamesco.jamboree.Util.requestAttentionIfNotActive
 
 object IslandGameStartNotify {
-    internal val Window.isActive: Boolean
-        get() = GLFW.glfwGetWindowAttrib(window, GLFW.GLFW_FOCUSED) != 0
-
-    internal fun Window.requestAttention() = GLFW.glfwRequestWindowAttention(window)
-
     fun registerListeners() {
         if (!FabricLoader.getInstance().isModLoaded("noxesium")) {
             logger.info("Not registering IslandGameStartNotify listeners, as Noxesium is not loaded")
@@ -29,11 +23,7 @@ object IslandGameStartNotify {
             }
 
             // We're joining a game server (doesn't count fishing, as those are classified as lobby servers)
-            Minecraft.getInstance().window.run {
-                if (!isActive) {
-                    requestAttention()
-                }
-            }
+            Minecraft.getInstance().window.requestAttentionIfNotActive()
         }
     }
 }
--- src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
@@ -1,8 +1,10 @@
 package uk.mrjamesco.jamboree
 
+import com.mojang.blaze3d.platform.Window
 import com.noxcrew.noxesium.NoxesiumFabricMod
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.fabricmc.loader.api.FabricLoader
+import org.lwjgl.glfw.GLFW
 import uk.mrjamesco.jamboree.Jamboree.Companion.logger
 
 object Util {
@@ -18,6 +20,17 @@ object Util {
         // Initialise Noxesium once if it's loaded
         if (FabricLoader.getInstance().isModLoaded("noxesium")) {
             NoxesiumFabricMod.initialize()
+        }
+    }
+
+    val Window.isActive: Boolean
+        get() = GLFW.glfwGetWindowAttrib(window, GLFW.GLFW_FOCUSED) != 0
+
+    fun Window.requestAttention() = GLFW.glfwRequestWindowAttention(window)
+
+    fun Window.requestAttentionIfNotActive() {
+        if (!isActive) {
+            requestAttention()
         }
     }
 }
