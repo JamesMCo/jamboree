ba0e2361ff16ac3ff3f74da7bf3f3336e6ec32a3
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-08-10 01:07:35 +0100

    Fix crash when launching Lunar Client with Jamboree loaded

--- src/main/java/uk/mrjamesco/jamboree/mixins/ChatComponentMixin.java
+++ src/main/java/uk/mrjamesco/jamboree/mixins/ChatComponentMixin.java
@@ -14,5 +14,5 @@ public interface ChatComponentMixin {
     List<GuiMessage> getAllMessages();
 
     @Invoker("refreshTrimmedMessages")
-    void refreshTrimmedMessages();
+    void refreshChat();
 }
