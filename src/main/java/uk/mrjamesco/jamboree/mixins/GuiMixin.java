ae8319ea321876aa927cb6d6942d13d2bab7ef96
James C. <3661681+JamesMCo@users.noreply.github.com>
2025-10-02 13:22:46 +0100

    Add hide scoreboard functionality
    When enabled, hides scoreboard from the sidebar when the player is in creative mode.
    (Useful for recording in creative mode when you don't want to be able to see the scoreboard, but do want to be able to see the rest of the UI)

---
+++ src/main/java/uk/mrjamesco/jamboree/mixins/GuiMixin.java
@@ -0,0 +1,23 @@
+package uk.mrjamesco.jamboree.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import uk.mrjamesco.jamboree.Config;
+
+@Mixin(Gui.class)
+public class GuiMixin {
+    @Inject(method = "displayScoreboardSidebar", at = @At("HEAD"), cancellable = true)
+    public void cancelDisplayScoreboardSidebarIfHidingScoreboard(CallbackInfo ci) {
+        if (Config.AdjustRendering.INSTANCE.getHideScoreboard()) {
+            MultiPlayerGameMode gameMode = Minecraft.getInstance().gameMode;
+            if (gameMode != null && gameMode.getPlayerMode().isCreative()) {
+                ci.cancel();
+            }
+        }
+    }
+}
