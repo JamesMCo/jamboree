e9ed26b14271547a89b49dfabfb9104972ae1e52
James C. <3661681+JamesMCo@users.noreply.github.com>
2026-01-06 00:53:54 +0000

    Adjust IslandGameStartNotify to check for "game" in type rather than exclude fishing

--- src/main/kotlin/uk/mrjamesco/jamboree/IslandGameStartNotify.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/IslandGameStartNotify.kt
@@ -19,12 +19,13 @@ object IslandGameStartNotify {
         JamboreeNoxEntrypoint.whenInitialized {
             MccPackets.CLIENTBOUND_MCC_SERVER.addListener(this, ClientboundMccServerPacket::class.java) noxesiumPacket@{ _, packet, _ ->
                 // Only consider scenarios where the config option is enabled,
-                // we're joining an Island server that isn't a lobby
-                if (!Config.IslandGameStartNotify.enabled || !Util.onMCCIsland || packet.server == "lobby" || packet.server == "fishing") {
+                // we're joining an Island server that isn't a lobby, and
+                // the server has "game" in its type
+                if (!Config.IslandGameStartNotify.enabled || !Util.onMCCIsland || packet.server == "lobby" || "game" !in packet.types) {
                     return@noxesiumPacket
                 }
 
-                // We're joining a game server (doesn't count fishing, as those are classified as lobby servers)
+                // We're joining a game server
                 Minecraft.getInstance().window.requestAttentionIfNotActive()
             }
         }
