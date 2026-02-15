6c17b25d089b72c517bf49e73668c96ced9a1963
James C. <3661681+JamesMCo@users.noreply.github.com>
2026-02-15 02:19:24 +0000

    Refactor animated Compact Fishing modes to use common super class

--- src/main/kotlin/uk/mrjamesco/jamboree/compactfishing/ReplacingXpSeparateFishingMessageHandler.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/compactfishing/ReplacingXpSeparateFishingMessageHandler.kt
@@ -1,74 +1,16 @@
 package uk.mrjamesco.jamboree.compactfishing
 
-import com.deflanko.MCCFishingMessages.MCCFishingMessagesMod
-import net.fabricmc.loader.api.FabricLoader
-import net.minecraft.client.GuiMessage
-import net.minecraft.client.Minecraft
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.MutableComponent
 import uk.mrjamesco.jamboree.Config
-import uk.mrjamesco.jamboree.compactfishing.CompactFishing.altIconOrder
-import uk.mrjamesco.jamboree.compactfishing.CompactFishing.isCaughtMessage
-import uk.mrjamesco.jamboree.compactfishing.CompactFishing.isIconMessage
-import uk.mrjamesco.jamboree.compactfishing.CompactFishing.isXPMessage
-import uk.mrjamesco.jamboree.mixins.mccfishingmessages.FishingChatBoxMixin
-import uk.mrjamesco.jamboree.mixins.minecraft.ChatComponentMixin
 
 /**
  * Sends initial caught messages immediately, and replaces
  * that message in place with new versions when icon messages
  * and XP messages are received.
  */
-object ReplacingXpSeparateFishingMessageHandler : FishingMessageHandler {
-    private var caughtMessage: MutableComponent? = null
-    private val iconBuffer: MutableList<Pair<MutableComponent, Int>> = mutableListOf()
-    private var xpMessage: MutableComponent? = null
+object ReplacingXpSeparateFishingMessageHandler : CommonReplacingFishingMessageHandler() {
     private lateinit var arrowPrefix: MutableComponent
-    private var sentMessageInVanillaChat: Boolean = true
-
-    override fun handleCaughtMessage(message: Component): Boolean {
-        caughtMessage = message.copy()
-
-        // Seeing a new caught message means we are handling a new set of messages
-        // Therefore, we need to clear the icons and xp message
-        iconBuffer.clear()
-        xpMessage = null
-
-        // Check whether the message is going to be pulled by the MCC Fishing Messages mod
-        sentMessageInVanillaChat = !(
-            FabricLoader.getInstance().isModLoaded("mcc-fishing-messages") &&
-            MCCFishingMessagesMod.fishingChatBox.isVisible
-        )
-
-        // Updating message as new info received immediately,
-        // so we need to let the initial message through
-        return true
-    }
-
-    override fun handleIconMessage(message: Component): Boolean {
-        if (Config.CompactFishing.showIcons) {
-            message
-                .siblings.first() // e.g. "[] Triggered: [] Supply Preserve"
-                .siblings.last()  // e.g. "Triggered: [] Supply Preserve"
-                .siblings         // e.g. "[]", " ", "Supply Preserve"
-                .let {
-                    iconBuffer.addLast(Pair(
-                        it.first().copy().apply {
-                            // Use existing style (e.g. font), but replace hover event with that of overall message
-                            if (message.siblings.first().style.hoverEvent !== null) {
-                                this.style = style.withHoverEvent(message.siblings.first().style.hoverEvent)
-                            }
-                        },
-                        altIconOrder(it.last().string)
-                    ))
-                }
-        }
-
-        // Updating message as new info received immediately,
-        // so we need to update chat and block the message
-        updateChat()
-        return false
-    }
 
     override fun handleXPMessage(message: Component): Boolean {
         // Ensure that we have a copy of the server-sent spacing and arrow icon to prefix our message
@@ -82,20 +24,10 @@ object ReplacingXpSeparateFishingMessageHandler : FishingMessageHandler {
             }
         }
 
-        if (Config.CompactFishing.showXP) {
-            message
-                .siblings.last()  // "You earned: n Island XP"
-                .siblings.last()  // "n Island XP"
-                .let {
-                    xpMessage = it.copy()
-                }
-        }
-
-        updateChat()
-        return false
+        return super.handleXPMessage(message)
     }
 
-    fun buildCompactMessage(): Component = caughtMessage?.copy()?.apply {
+    override fun buildCompactMessage(): Component = caughtMessage?.copy()?.apply {
         if (iconBuffer.isNotEmpty()) {
             append(Component.literal(" "))
 
@@ -113,53 +45,4 @@ object ReplacingXpSeparateFishingMessageHandler : FishingMessageHandler {
             append(xpMessage!!)
         }
     } ?: Component.empty()
-
-    fun updateChat() {
-        // Update chat by replacing an existing message
-
-        // Only need to toggle sendingCompactMessage when sending a new message
-        // (replacing an existing message doesn't trigger ClientReceiveMessageEvents.ALLOW_GAME)
-        if (FabricLoader.getInstance().isModLoaded("mcc-fishing-messages")) {
-            (MCCFishingMessagesMod.fishingChatBox as FishingChatBoxMixin).apply replaceExistingCatchMessage@{
-                messages.forEach { messageContainer ->
-                    val message = messageContainer.chatHudLine
-                    if (message.content.isCaughtMessage()) {
-                        // Compact messages start with caught messages, so will match the same regex
-                        messageContainer.chatHudLine = GuiMessage(
-                            message.addedTime,
-                            buildCompactMessage(),
-                            message.signature,
-                            message.tag
-                        )
-                        return@replaceExistingCatchMessage
-                    }
-                }
-            }
-        }
-        if (sentMessageInVanillaChat) {
-            (Minecraft.getInstance().gui.chat as ChatComponentMixin).apply replaceExistingCatchMessage@{
-                allMessages.forEachIndexed { i, message ->
-                    if (message.content.isCaughtMessage()) {
-                        // Compact messages start with caught messages, so will match the same regex
-                        allMessages[i] = GuiMessage(
-                            message.addedTime,
-                            buildCompactMessage(),
-                            message.signature,
-                            message.tag
-                        )
-                        refreshChat()
-                        return@replaceExistingCatchMessage
-                    }
-                }
-            }
-        }
-    }
-
-    override fun maybeAlterMCCFishingMessages(message: Component): Boolean? =
-        when {
-            message.isCaughtMessage() -> true
-            message.isIconMessage() -> false
-            message.isXPMessage() -> false
-            else -> null
-        }
 }
