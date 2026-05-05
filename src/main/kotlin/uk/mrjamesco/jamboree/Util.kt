c152a16179a60eab07c7c587e10c664cd7c93ea3
James C. <3661681+JamesMCo@users.noreply.github.com>
2026-05-05 16:22:43 +0100

    Remove MCCI server join detection
    As this version has no MCCI functionality, there isn't any reason to detect joining the server

--- src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
+++ src/main/kotlin/uk/mrjamesco/jamboree/Util.kt
@@ -1,19 +1,12 @@
 package uk.mrjamesco.jamboree
 
 import com.mojang.blaze3d.platform.Window
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import org.lwjgl.glfw.GLFW
 import uk.mrjamesco.jamboree.Jamboree.Companion.logger
 
 object Util {
-    var onMCCIsland: Boolean = false
-        private set
-
     fun registerListeners() {
         logger.info("Registering Util listeners")
-
-        // Detect joining MCC Island
-        ClientPlayConnectionEvents.JOIN.register { handler, _, _ -> onMCCIsland = Regex("mccisland\\.(net|com)").containsMatchIn(handler.connection.remoteAddress.toString()) }
     }
 
     val Window.isActive: Boolean
